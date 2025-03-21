144b6927f395a7a2f2a3157ef7bd6cec36be86f2
Raúl Rodríguez <raul.rodriguez.c@outlook.com>
2025-03-21 10:08:33 +0100

    add insurance and strech of days

---
+++ src/api/insurance-category/controllers/insurance-category.ts
@@ -0,0 +1,7 @@
+/**
+ * insurance-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::insurance-category.insurance-category');
---
+++ src/api/insurance-category/routes/insurance-category.ts
@@ -0,0 +1,7 @@
+/**
+ * insurance-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::insurance-category.insurance-category');
